0def4444c60d3ca83317a831c7bc022ff6012c0d
Jerico De Guzman <jjddeguzman@gmail.com>
2020-11-16 22:35:11 +0800

    FEAT: Import item interface, service and add constructor to get data

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto'
+import { Item } from './interfaces/item.interface'
+import { ItemsService } from './items.service'
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService: ItemsService){}
     @Get()
-    findAll(): string{
-        return 'Get All Items';
+    findAll(): Item[]{
+        return this.itemsService.findAll();
     }
 
     @Post()
