a8a96bb7fd3e756fba08124d43fa7eeed8cfdc04
Jerico De Guzman <jjddeguzman@gmail.com>
2020-11-16 22:34:07 +0800

    CHORE : Add hardcoded data to get all items

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface'
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [
+        {
+            id: "23423423423",
+            name: "Item One",
+            description: "This is item one",
+            qty: 100,
+        },
+        {
+            id: "423423",
+            name: "Item Two",
+            description: "This is item two",
+            qty: 200,
+        },        
+    ]
+
+    findAll(): Item[]{
+        return this.items;
+    }
+}
